a5063e4982b237cbbd6f92bfd64f488740d8a136
aminnak <aminamirikeivani@gmail.com>
2025-03-15 01:40:57 +0330

    Add the dynamic user post's template ( the user's journey template which will be retirved from the backend) && Change some minor styles

--- frontend/src/pages/home.tsx
+++ frontend/src/pages/home.tsx
@@ -1,11 +1,15 @@
-import Header from "../components/header"
-
+import Header from "../components/Header"
+import valley from '../assets/images/valley.jpg'
+import Journey from "../components/Journey"
 
 const home = () => {
   return (
-    <>
-        <Header />
-    </>
+    <div className='bg-cover bg-center bg-fixed' style={{backgroundImage : `url(${valley})`}}>
+        <div className="bg-black/40">
+            <Header />
+            <Journey />
+        </div>
+    </div>
   )
 }
 
