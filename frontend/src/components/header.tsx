0a944524c94be51ec0565c36f24c41f4ee6051a0
aminnak <aminamirikeivani@gmail.com>
2025-03-15 01:38:30 +0330

    Complete the Header section

--- frontend/src/components/header.tsx
+++ frontend/src/components/header.tsx
@@ -1,15 +1,87 @@
-import valley from '../assets/images/valley.jpg'
-import Nav from './nav'
+// import valley from '../assets/images/valley.jpg'
+import DateCard from '../cards/DateCard'
+import ListItem from '../cards/ListItem'
+import LocationCard from '../cards/LocationCard'
+import UserCard from '../cards/UserCard'
+import Nav from './Nav'
 
 const Header = () => {
+    const RobotoFont = {
+        fontFamily: "Roboto, sans-serif",
+    }
+
   return (
-    <div className='bg-cover bg-center' style={{backgroundImage : `url(${valley})`}}>
-        <header className="flex flex-col justify-between items-start h-full w-full bg-black/40 rounded" >
+
+        <header className="flex flex-col justify-between items-start w-full rounded" >
+            {/* first part: Navbar */}
             <Nav />
-            <div className='text-white'>some text</div>
-            <div className='text-white'>some another text</div>
+            {/* second part: the title on the center of the center of the header */}
+            <div className='flex flex-col justify-center items-center w-full text-white text-center space-y-4 py-16 max-lg:min-h-[500px] min-h-[350px]'>
+                <h1 className='max-w-[500px] text-5xl text-gray-200 font-bold font-tahoma'>Discover Your Next Great Adventure</h1>
+                <h3 className='max-w-[470px] text-lg text-gray-100/60 font-medium' style={RobotoFont}>
+                    Immerse yourself in a extraordinary with us takes you on a journey to uncover the world's hidden gems
+                </h3>
+            </div>
+            {/* third part: the list of items */}
+            <div className='w-full md:px-8 px-3'>
+                <div className='flex flex-col w-full bg-stone-100/60 rounded-lg my-6 md:px-4 py-6'>
+                    {/* title and navigation links */}
+                    <div className='flex justify-between items-center max-lg:flex-col max-lg:space-y-6'>
+                        <h3 className='text-2xl font-semibold '>
+                            Explore people's journeys
+                        </h3>
+                        <ul className='flex space-x-2.5'>
+                            <ListItem
+                                backgroundColor='bg-black'
+                                title='Personal'
+                                textColor='text-slate-100'
+                                padding='px-4'
+                            />
+                            <ListItem title='Cultural'/>
+                            <ListItem title='Adventure'/>
+                            <ListItem title='Historical'/>
+                            <ListItem title='Spiritual'/>
+                        </ul>
+                    </div>
+                    {/* inputs */}
+                    <div className='flex w-full gap-4 mt-6 max-lg:flex-wrap max-lg:items-center max-lg:justify-center max-md:flex-col'>
+                        <div className='flex flex-col w-3/8 space-y-1 max-lg:w-5/12 max-md:w-full max-md:px-4'>
+                            <h4 className='font-semibold text-black/80' style={RobotoFont}>Country</h4>
+                            <div className='flex items-center w-full bg-gray-200/60 rounded-md inset-shadow-sm'>
+                                <span className='pl-2 pr-1'>
+                                    <LocationCard />
+                                </span>
+                                <input type="text" className='w-full py-3 px-3 focus:outline-none focus:inset-shadow-sm rounded text-gray-700 placeholder:text-gray-700/70' placeholder='United State'/>
+                            </div>
+                        </div>
+                        <div className='flex flex-col w-2/8 space-y-1 max-lg:w-5/12 max-md:w-full max-md:px-4'>
+                            <h4 className='font-semibold text-black/80' style={RobotoFont}>Year</h4>
+                            <div className='flex items-center w-full bg-gray-200/60 rounded-md inset-shadow-sm'>
+                                <span className='pl-2 pr-1'>
+                                    <DateCard />
+                                </span>
+                                <input type="text" className='w-full py-3 px-3 focus:outline-none focus:inset-shadow-sm rounded text-gray-700 placeholder:text-gray-700/70' placeholder='Enter a year (e.g., 2005)'/>
+                            </div>
+                        </div>
+                        <div className='flex flex-col w-3/8 space-y-1 max-lg:w-5/12 max-md:w-full max-md:px-4'>
+                            <h4 className='font-semibold text-black/80' style={RobotoFont}>By Publisher</h4>
+                            <div className='flex items-center w-full bg-gray-200/60 rounded-md inset-shadow-sm'>
+                                <span className='pl-2 pr-1'>
+                                    <UserCard />
+                                </span>
+                                <input type="text" className='w-full py-3 px-3 focus:outline-none focus:inset-shadow-sm text-gray-700 placeholder:text-gray-700/70 rounded' placeholder='Aminnak'/>
+                            </div>
+                        </div>
+                    </div>
+                    {/* search button */}
+                    <div className='flex justify-end items-center mt-5 mb-2 pr-6'>
+                        <button className='bg-black text-white py-2 px-4 rounded-md font-semibold focus:scale-95 duration-300'>
+                            Search
+                        </button>
+                    </div>
+                </div>
+            </div>
         </header>
-    </div>
   )
 }
 
--- frontend/src/components/nav.tsx
+++ frontend/src/components/nav.tsx
@@ -1,35 +1,56 @@
-import EarthSvg from "../cards/earthCard"
-import SearchCard from "../cards/searchCard"
+import EarthSvg from "../cards/EarthCard"
+import SearchCard from "../cards/SearchCard"
 
 const Nav = () => {
-
+    // creating the list items in navbar dinamically
     const NavListItemTitles = ['Hotel','Flight','Train','Travel','Car Rental']
     const NavListItems = NavListItemTitles.map((title ,index)=> (
-        <li key={index} className="hover:text-neutral-400 hover:cursor-pointer hover:scale-97 duration-300">{title}</li>
+        <li
+            key={index}
+            className="hover:text-neutral-400 hover:cursor-pointer hover:scale-97 duration-300 max-md:hidden"
+        >
+            {title}
+        </li>
     ))
 
   return (
+    // Navbar which include three parts (icon and navigation links, search bar, login and sign up buttons)
     <nav className="flex justify-between items-center w-full text-slate-50 px-5 mt-3">
+        {/*  first part: icon and navigation links */}
         <div className="flex">
             <ul className="flex justify-center items-center w-full font-semibold list-none space-x-7">
-                <li className="flex items-center group space-x-1">
+                <li className="flex items-center group space-x-1 hover:scale-97 duration-300">
                     <span className="w-[35px] ">
                         <EarthSvg />
                     </span>
-                    <h3 className="group-hover:text-neutral-400 duration-300">Horiza</h3>
+                    <h3 className="group-hover:text-neutral-400 duration-300 max-lg:hidden">Horiza</h3>
                 </li>
                 {NavListItems}
             </ul>
         </div>
-        <div className="flex justify-center items-center w-1/4 bg-gray-50/50 rounded">
-            <input className="w-full rounded  py-1.5 px-3 placeholder:text-stone-100 focus:outline-1 focus:outline-gray-100" placeholder="Serach destination..."/>
-            <span className="px-1">
+        {/*  second part: search bar */}
+        <div className="flex justify-center items-center w-1/4 bg-gray-50/50 rounded max-md:hidden">
+            <input
+                placeholder="Serach destination..."
+                className="
+                    w-full rounded py-1.5 px-3 placeholder:text-stone-100
+                    focus:outline-1 focus:outline-gray-100"
+            />
+            <span className="px-1 md:hidden">
+                <SearchCard />
+            </span>
+            <span className="px-1 md:block">
                 <SearchCard />
             </span>
         </div>
+        {/*  third part: login and sign up buttons */}
         <div className="flex space-x-6">
-            <button className="font-medium text-lg">Log In</button>
-            <button className="bg-gray-100 text-black rounded-md py-1.5 px-3 font-medium">Sign Up</button>
+            <button className="font-medium text-lg">
+                Log In
+            </button>
+            <button className="bg-gray-100 text-black rounded-md py-1.5 px-3 font-medium">
+                Sign Up
+            </button>
         </div>
     </nav>
   )
