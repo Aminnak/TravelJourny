489424816bbd6bb9cfa2a15ee823b56f428bbf8d
aminnak <aminamirikeivani@gmail.com>
2025-03-21 00:57:28 +0330

    Add the base form implementaion (useForm) (child and parent relation for useForm)

--- frontend/src/components/FileInput.tsx
+++ frontend/src/components/FileInput.tsx
@@ -1,16 +1,38 @@
-import React, { useState } from "react";
-
+import { useRef, useState } from "react";
+import XIcon from "../iconCards/XIcon";
+import FullscreenCard from "../iconCards/FullscreenCard";
+import { FieldErrors, UseFormRegister } from "react-hook-form";
+import { mainFormInterface } from "../pages/CreatePost";
 interface FileInputProps {
     maxSize? : number;
-    onFileSelected? : (file : File) => void;
+    register : UseFormRegister<mainFormInterface>;
+    errors : FieldErrors<mainFormInterface>
+    onFileSelected : (file : File) => void;
 }
-const FileInput : React.FC<FileInputProps> = ({maxSize = 3 * 1024 * 1024 , onFileSelected}) => {
+const FileInput : React.FC<FileInputProps> = ({maxSize = 3 * 1024 * 1024 , onFileSelected , register , errors}) => {
     const [fileName , setFileName] = useState<string>('');
     const [fileSize , setFileSize] = useState<string>('');
     const [error , setError] = useState<String>('');
     const [imgWidth , setImgWidth] = useState<null | number>(null);
     const [imgHeight , setImgHeight] = useState<null | number>(null);
     const [imgPreview , setImgPreview] = useState<null | string>(null);
+
+    const userUploadedImgRef = useRef<HTMLImageElement>(null)
+
+    const openFullscreen = () => {
+        if(userUploadedImgRef.current?.requestFullscreen){
+            userUploadedImgRef.current.requestFullscreen()
+        }
+    }
+
+    const clearFile = () : void => {
+        setFileName('');
+        setFileSize('');
+        setError('');
+        setImgWidth(null);
+        setImgHeight(null);
+        setImgPreview(null);
+    }
 
     const handleFileChange = (event : React.ChangeEvent<HTMLInputElement>) => {
         const file = event.target.files?.[0]
@@ -48,68 +70,78 @@ const FileInput : React.FC<FileInputProps> = ({maxSize = 3 * 1024 * 1024 , onFil
         const reader = new FileReader();
         reader.onload = (e) => {
             const image = new Image();
+            image.src = e.target?.result as string;
             image.onload = () => {
                 setImgHeight(image.height);
                 setImgWidth(image.width);
-                if (onFileSelected) {
-                    onFileSelected(file);
-                }
+                onFileSelected(file);
             }
-            image.src = e.target?.result as string;
             setImgPreview(e.target?.result as string);
         }
         reader.readAsDataURL(file);
     }
   return (
-    <div className="flex items-center justify-center w-full">
-      <label
-        htmlFor="dropzone-file"
-        className="flex flex-col items-center justify-center w-full h-64 border-2 border-gray-300 border-dashed rounded-lg cursor-pointer bg-gray-50 dark:hover:bg-gray-800 dark:bg-gray-700 hover:bg-gray-100 dark:border-gray-600 dark:hover:border-gray-500 dark:hover:bg-gray-600"
-      >
-        <div className="flex flex-col items-center justify-center pt-5 pb-6">
-          <svg
-            className="w-8 h-8 mb-4 text-gray-500 dark:text-gray-400"
-            aria-hidden="true"
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 20 16"
-          >
-            <path
-              stroke="currentColor"
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              strokeWidth="2"
-              d="M13 13h3a3 3 0 0 0 0-6h-.025A5.56 5.56 0 0 0 16 6.5 5.5 5.5 0 0 0 5.207 5.021C5.137 5.017 5.071 5 5 5a4 4 0 0 0 0 8h2.167M10 15V6m0 0L8 8m2-2 2 2"
-            />
-          </svg>
-          <p className="mb-2 text-sm text-gray-500 dark:text-gray-400">
-            <span className="font-semibold">Click to upload</span> or drag and drop
-          </p>
-          <p className="text-xs text-gray-500 dark:text-gray-400">SVG, PNG, JPG or GIF (MAX. 800x400px)</p>
-        </div>
-        <input id="dropzone-file" type="file" className="hidden" onChange={handleFileChange} accept="image/*" />
-      </label>
+    <div className="flex flex-col items-center justify-center w-full">
+      <div className="h-70 w-full">
+        {!fileName ?
+            <><label
+                htmlFor="dropzone-file"
+                className="flex flex-col items-center justify-center w-full h-full border border-stone-300 rounded-lg cursor-pointer  hover:bg-black/50 duration-300"
+            >
+                <div className="flex flex-col items-center justify-center pt-5 pb-6">
+                    <svg
+                        className="w-8 h-8 mb-4 text-gray-500 dark:text-gray-400"
+                        aria-hidden="true"
+                        xmlns="http://www.w3.org/2000/svg"
+                        fill="none"
+                        viewBox="0 0 20 16"
+                    >
+                        <path
+                        stroke="currentColor"
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        strokeWidth="2"
+                        d="M13 13h3a3 3 0 0 0 0-6h-.025A5.56 5.56 0 0 0 16 6.5 5.5 5.5 0 0 0 5.207 5.021C5.137 5.017 5.071 5 5 5a4 4 0 0 0 0 8h2.167M10 15V6m0 0L8 8m2-2 2 2"
+                        />
+                    </svg>
+                    <p className="mb-2 text-sm text-gray-500 dark:text-gray-400">
+                        <span className="font-semibold">Click to upload</span> or drag and drop
+                    </p>
+                    <p className="text-xs text-gray-500 dark:text-gray-400">SVG, PNG, JPG or GIF (MAX. 800x400px)</p>
+                </div>
+                <input {...register("file" , {required : 'This field is required' , validate : (value) => {
+                    return value.size ? 'fuck u' : true // we will complete it later
+                }})} id="dropzone-file" type="file" className="hidden" onChange={handleFileChange} accept="image/*" />
+            </label>
+            {errors.file && <p className="text-red-600">{errors.file.message}</p>}
+            </> : imgPreview && (
+                <div className="relative">
+                  <img ref={userUploadedImgRef} src={imgPreview} alt="Preview" className="w-full md:h-full rounded-md" />
+                  <span onClick={clearFile} className="absolute right-1 top-1 w-[20px] h-[20px] hover:cursor-pointer pr-2 pt-2">
+                    <XIcon />
+                  </span>
+                  <span onClick={() => openFullscreen()} className="absolute right-2 bottom-2 w-[20px] h-[20px] opacity-100 scale-100 duration-300 hover:cursor-pointer group-hover:opacity-100 group-hover:scale-100"><FullscreenCard /></span>
+                </div>
+              )
+    }
+      </div>
       {error ? (
         <div className="text-red-500 mt-2">{error}</div>
       ) : (
         <div className="text-gray-600 mt-2">
           {fileName ? (
             <div>
-              <span>{fileName}</span>
-              <div>Size: {fileSize}</div>
+              <span className="text-gray-200">{fileName}</span>
+              <div className="text-gray-200">Size: {fileSize}</div>
               {imgWidth !== null && imgHeight !== null && (
-                <div>
+                <div className="text-gray-200">
                   Dimensions: {imgWidth}x{imgHeight}
                 </div>
               )}
-              {imgPreview && (
-                <div className="m-12">
-                  <img src={imgPreview} alt="Preview" className="w-48 h-auto" />
-                </div>
-              )}
+
             </div>
           ) : (
-            <span>No image chosen</span>
+            <span className="text-gray-200">No image chosen</span>
           )}
         </div>
       )}
--- frontend/src/pages/CreatePost.tsx
+++ frontend/src/pages/CreatePost.tsx
@@ -1,10 +1,70 @@
 import FileInput from '../components/FileInput'
+import JourneyExprience from '../assets/images/JourneyExprience.jpg'
+import {SubmitHandler , useForm } from 'react-hook-form'
+
+export interface mainFormInterface {
+    file : File;
+    googleMapLink : string;
+    title : string;
+    location : string;
+    description : string;
+    year : number;
+}
 
 const CreatePost = () => {
+
+    const { register , handleSubmit , setValue , formState : {errors , isSubmitting} } = useForm<mainFormInterface>()
+
+    const handleFileInput = (file : File) => setValue('file' , file)
+
+    const formSubmitHandler : SubmitHandler<mainFormInterface> = (data) => console.log(data)
   return (
-    <div className='flex justify-start w-1/3 p-6'>
-      <FileInput />
-    </div>
+    <form onSubmit={handleSubmit(formSubmitHandler)} className='w-full bg-fixed bg-cover bg-center' style={{backgroundImage : `url(${JourneyExprience})`}}>
+        <div className="flex flex-col w-full p-6">
+            <div className='rounded-md bg-black/70 min-h-screen'> {/* might cuase display issue in here*/}
+                <div className='flex max-md:flex-col-reverse w-full space-x-12 p-6'>
+                    <div className='w-4/9 max-md:w-full max-md:mt-8'>
+                        <FileInput onFileSelected={handleFileInput} register={register} errors={errors}/>
+                    </div>
+                    <div className='flex flex-col w-5/9 max-md:w-full space-y-4'>
+                        <div className="flex flex-col space-y-2">
+                            <label htmlFor='' className='text-white font-semibold'>Title</label>
+                            <input {...register('title', {required : 'This filed is required.' , minLength : {value : 8 , message : 'Title has to be more than 8 characters.'}})} className='w-full px-4 py-2 rounded-md inset-shadow-sm inset-shadow-black bg-stone-400 text-black focus:outline-none placeholder:text-black/65' type="text" placeholder='Enter your journey title'/>
+                            {errors.title && <p className='text-red-600'>{errors.title.message}</p>}
+                        </div>
+                        <div className="flex flex-col space-y-2">
+                            <label htmlFor='' className='text-white font-semibold'>Location</label>
+                            <input {...register('location' , {required : 'This field is required.' , minLength : {value : 2 , message : 'Location has to be more than 2 characters'}})} className='w-full px-4 py-2 rounded-md inset-shadow-sm inset-shadow-black bg-stone-400 text-black focus:outline-none placeholder:text-black/65' type="text" placeholder='Locate the place'/>
+                            {errors.location && <p className='text-red-600'>{errors.location.message}</p>}
+                        </div>
+                        <div className="flex w-full max-md:flex-col space-x-2">
+                            <div className="flex flex-col w-6/10 max-md:w-full space-y-2">
+                                <label htmlFor='' className='text-white font-semibold'>Google map link</label>
+                                <input {...register('googleMapLink' , {required : 'This field is required.' , pattern : {value : /^(https?:\/\/)?(www\.)?maps\.google\.[a-zA-Z]{2,3}(\/maps)?(\?.*|\/place\/.*|\/dir\/.*)?$/ , message : 'Invalid link.'}})} className='px-4 py-2 rounded-md inset-shadow-sm inset-shadow-black bg-stone-400 text-black focus:outline-none placeholder:text-black/65' type="text" placeholder='https://maps.google.com'/>
+                                {errors.googleMapLink && <p className='text-red-600'>{errors.googleMapLink.message}</p>}
+                            </div>
+                            <div className="flex flex-col w-4/10 max-md:w-4/6 space-y-2">
+                                <label htmlFor='' className='text-white font-semibold'>Year</label>
+                                <input {...register('year' , {required : 'This field is required.' , pattern : {value : /^\d{4}$/ , message : 'Enter a valid year(e.g. 2007).'}})} className='px-4 py-2 rounded-md inset-shadow-sm inset-shadow-black bg-stone-400 text-black focus:outline-none placeholder:text-black/65' type="text" placeholder='Enter a year (e.g., 2009)'/>
+                                {errors.year && <p className='text-red-600'>{errors.year.message}</p>}
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div className='w-full justify-center px-16 max-md:px-6 py-4'>
+                    <article className='flex flex-col w-full space-y-4'>
+                        <textarea {...register('description' , {required : 'This field is required.' ,minLength : {value : 50 , message : 'Description has to be at least 50 characters long.'}})} className='w-full px-4 py-3 focus:outline-none min-h-75 max-md:h-90 rounded-md inset-shadow-sm font-semibold inset-shadow-black bg-stone-300/80 text-gray-950/50 placeholder:text-gray-900/50'
+                            placeholder="With hope in my heart, the adventure begins..."
+                        />
+                        {errors.description && <p className='text-red-600'>{errors.description.message}</p>}
+                        <div className='flex justify-end'>
+                            <button type='submit' className='px-4 py-1.5 rounded-md bg-blue-600 text-white font-semibold hover:cursor-pointer'>Submit</button>
+                        </div>
+                    </article>
+                </div>
+            </div>
+        </div>
+    </form>
   )
 }
 
