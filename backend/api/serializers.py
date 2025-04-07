c810806d2899a55aad905d0f05eb4c92ead73b47
aminnak <aminamirikeivani@gmail.com>
2025-04-08 02:48:58 +0330

    Add the new fields to the serializer such as username ,user profile and etc... && Add email validator && Add the methods which manage some fields for instance the one provide the full url for files , or user related fields which needed to be change in api.

--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -1,15 +1,17 @@
 from rest_framework import serializers
 from django.core.validators import EmailValidator
 from django.contrib.auth import get_user_model
+from django.utils.timezone import localtime
 from .models import TravelPostModel
 User = get_user_model()
 
 class CustomUserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = ['email' , 'password']
+        fields = ['email' , 'password', 'profile' , 'username']
         extra_kwargs = {
             'password' : {'write_only' : True , 'min_length' : 8},
+            'username' : {'required' : True},
             'email' : {
                 'write_only' : True ,
                 'required' : True ,
@@ -17,6 +19,11 @@ class CustomUserSerializer(serializers.ModelSerializer):
                 'validators' : [EmailValidator()]
             }
         }
+
+    def validate_email(self, value):
+        if User.objects.filter(email=value).exists():
+            raise serializers.ValidationError("Email already exists. Try logging in or using another email.")
+        return value
 
     def create(self, validated_data):
         password = validated_data.pop('password')
@@ -27,9 +34,12 @@ class CustomUserSerializer(serializers.ModelSerializer):
 
 
 class TravelPostSerializer(serializers.ModelSerializer):
+    published_at = serializers.SerializerMethodField()
+    user_profile = serializers.SerializerMethodField()
+    username = serializers.SerializerMethodField()
     class Meta:
         model = TravelPostModel
-        fields = ['title' , 'location' , 'google_map_link' , 'year' , 'picture' , 'description']
+        fields = ['id' ,'title' , 'location' , 'google_map_link' , 'year' , 'picture' , 'description' , 'published_at' , 'username' , 'user_profile']
         extra_kwargs = {
             'picture' : {
                 'max_length' : 100,
@@ -37,6 +47,18 @@ class TravelPostSerializer(serializers.ModelSerializer):
                 'required' : True
             },
         }
+
+    def get_username(self , obj):
+        return obj.user.username
+
+    def get_user_profile(self, obj):
+        request = self.context.get('request')
+        if obj.user.profile:
+            return request.build_absolute_uri(obj.user.profile.url)
+        return "default-profile-url"
+
+    def get_published_at(self , obj):
+        return localtime(obj.created_at).strftime("%B %d, %Y")
 
     def create(self, validated_data):
         user = self.context['request'].user  # Get user from request
